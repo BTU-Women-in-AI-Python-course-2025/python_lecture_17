8e87e4ba04fc628146ccbf7e90259e2514cc53f7
MariamKipshidze <mari.kifshidze@gmail.com>
2025-09-29 20:25:48 +0400

    Add BlogPostListCreateView

--- Lecture code 1/blog_post/blog/views.py
+++ Lecture code 1/blog_post/blog/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from blog.models import BlogPost
 from blog.serializers import BlogPostSerializer
@@ -22,6 +23,20 @@ def blog_post_list_create(request):
         return Response(serializer.data)
 
     elif request.method == 'POST':
+        serializer = BlogPostSerializer(data=request.data)
+        if serializer.is_valid():
+            BlogPost.objects.create(**serializer.validated_data)
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BlogPostListCreateView(APIView):
+    def get(self, request):
+        books = BlogPost.objects.filter(deleted=False)
+        serializer = BlogPostSerializer(books, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
         serializer = BlogPostSerializer(data=request.data)
         if serializer.is_valid():
             BlogPost.objects.create(**serializer.validated_data)
