1689d6fe40e42b217c3ecfa6f91364e8d7694932
MariamKipshidze <mari.kifshidze@gmail.com>
2025-09-30 20:29:35 +0400

    Add BlogPostDetailUpdateDeleteView and blog_post_detail_update_delete

--- Lecture code 2/blog_post/blog/urls.py
+++ Lecture code 2/blog_post/blog/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from blog.views import create_blog_post, blog_post_list_create, BlogPostListCreateView
+from blog.views import (
+    create_blog_post,
+    blog_post_list_create,
+    blog_post_detail_update_delete,
+    BlogPostListCreateView,
+    BlogPostDetailUpdateDeleteView
+)
 
 urlpatterns = [
     path('create_blog_post/', create_blog_post, name='create_blog_post'),
     path('blog_post_list_create/', blog_post_list_create, name='blog_post_list_create'),
     path('blog_post_list_create_class/', BlogPostListCreateView.as_view(), name='blog_post_list_create_class'),
+    path('blog_post_detail_update_delete/<int:id>/', blog_post_detail_update_delete, name='blog_post_detail_update_delete'),
+    path('blog_post_detail_update_delete_class/<int:id>/', BlogPostDetailUpdateDeleteView.as_view(), name='blog_post_detail_update_delete_class'),
 ]
--- Lecture code 2/blog_post/blog/views.py
+++ Lecture code 2/blog_post/blog/views.py
@@ -30,6 +30,33 @@ def blog_post_list_create(request):
             return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def blog_post_detail_update_delete(request, id):
+    if request.method == 'GET':
+        book = BlogPost.objects.filter(id=id).first()
+        if not book:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = BlogPostSerializer(book)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = BlogPostSerializer(data=request.data)
+        if serializer.is_valid():
+            book = BlogPost.objects.filter(id=id)
+            if not book:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+            BlogPost.objects.filter(id=id).update(**serializer.validated_data)
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        book = BlogPost.objects.filter(id=id).first()
+        if not book:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        book.deleted = True
+        book.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class BlogPostListCreateView(APIView):
     def get(self, request):
@@ -43,3 +70,30 @@ class BlogPostListCreateView(APIView):
             BlogPost.objects.create(**serializer.validated_data)
             return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BlogPostDetailUpdateDeleteView(APIView):
+    def get(self, request, id):
+        book = BlogPost.objects.filter(id=id).first()
+        if not book:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = BlogPostSerializer(book)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        serializer = BlogPostSerializer(data=request.data)
+        if serializer.is_valid():
+            book = BlogPost.objects.filter(id=id)
+            if not book:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+            BlogPost.objects.filter(id=id).update(**serializer.validated_data)
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        book = BlogPost.objects.filter(id=id).first()
+        if not book:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        book.deleted = True
+        book.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
